564c122142a0574c1d5a6c0127d4ff3c60cf5835
Jeremy <valancejeremy@gmail.com>
2020-12-27 15:43:58 -0700

    Added compliance manifest script

---
+++ scripts/compliance-manifest.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import os
+import json
+import yaml
+import re
+from pathlib import Path
+
+def main(): 
+    # Load compliance manifest yaml
+    compliance_manifest_yaml_path = Path("compliance_manifest.yaml")
+    if compliance_manifest_yaml_path.exists():
+        with open("compliance_manifest.yaml", 'r') as stream:
+            try:
+                content = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+    else:
+        print("Could not find hardening manifest file in repository root directory")
+
+    process_manifest(content)
+
+def process_manifest(content):
+    content["gitSha"] = os.getenv('GITHUB_SHA')
+    # Create JSON manifest file for reports stage
+    with open("artifacts/compliance-manifest.json", "w") as file:
+        json.dump(content, file)
+    
+if __name__ == "__main__":
+    main()
