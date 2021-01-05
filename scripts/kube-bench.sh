e550d287aa1a9027dfc3dc78bbd2b4fdee5f7ac7
Jeremy <valancejeremy@gmail.com>
2021-01-05 16:32:05 -0700

    Updated script

--- scripts/kube-bench.sh
+++ scripts/kube-bench.sh
@@ -11,7 +11,7 @@ echo "Attempting to update kubeconfig for aws"
 aws eks --region "$AWS_REGION" update-kubeconfig --name "$CLUSTER_NAME"
 kubectl apply -f kustomize/base/tools/job-eks.yaml
 
-while [[ $(kubectl get pods -l job-name=kube-bench -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" && sleep 1; done
+while [[ $(kubectl get job kube-bench -o 'jsonpath={..status.conditions[?(@.type=="Complete")].status}') != "True" ]]; do echo "waiting for job" && sleep 1; done
 
 POD=$(kubectl get pod -l job-name=kube-bench -o jsonpath="{.items[0].metadata.name}")
 kubectl logs $POD > kube-bench-report.json
