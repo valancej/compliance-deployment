262142f58ff966e4c1bea10e80efe0a00cc9ef61
Jeremy <valancejeremy@gmail.com>
2021-01-06 09:14:09 -0700

    Updated script

--- scripts/kube-bench.sh
+++ scripts/kube-bench.sh
@@ -14,5 +14,5 @@ kubectl apply -f kustomize/base/tools/job-eks.yaml
 while [[ $(kubectl get job kube-bench -o 'jsonpath={..status.conditions[?(@.type=="Complete")].status}') != "True" ]]; do echo "waiting for job" && sleep 1; done
 
 POD=$(kubectl get pod -l job-name=kube-bench -o jsonpath="{.items[0].metadata.name}")
-kubectl logs $POD > kube-bench-report.json
-ls
+kubectl logs $POD > artifacts/kube-bench-report.json
+ls -R
